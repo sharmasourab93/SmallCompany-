076d6598b3e42288a37c2319a30471dec9b2eaa7
Sourab <sharmasourab93@gmail.com>
2020-09-07 22:34:19 +0530

    Analytics Forms

--- SmallCompany/analytics/forms.py
+++ SmallCompany/analytics/forms.py
@@ -1,14 +1,17 @@
-from upload.models import DriverDetails
+from upload.models import DriverDetails, FuelType
 from django import forms
 
 
 class DriverSpendForm(forms.Form):
-
+        
     driver = forms.ModelChoiceField(
-        queryset=DriverDetails.objects.all())
+        queryset=DriverDetails.objects.all(),
+        empty_label="Select Driver"
+        )
     
 
 class FuelSpendForm(forms.Form):
     fuel_spend = forms.ModelChoiceField(
-        queryset=DriverDetails.objects.all()
+        queryset=FuelType.objects.all(),
+        empty_label="Select Fuel",
         )
